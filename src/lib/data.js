d3e7088cb03c684501f35ce3d9b813777db9b6b6
Brendan Malycky <bmalycky@hotmail.com>
2023-11-06 19:08:21 -0700

    created card component file and data file

---
+++ src/lib/data.js
@@ -0,0 +1,24 @@
+const albumData = [
+  {
+  title: "Ride The Lightning", 
+  description: "The 2nd album by metallica, released 1984",
+  src: "",
+  alt: "an image of metallicas 2nd album",
+  href: "https://www.metallica.com/"
+    },
+  {
+  title: "Master Of Puppets", 
+  description: "The 2nd album by metallica, released 1984",
+  src: "",
+  alt: "an image of metallicas 3rd album",
+  href: "https://www.metallica.com/"
+    },
+  {
+    title: "...And Justice For All", 
+    description: "The 2nd album by metallica, released 1984",
+    src: "",
+    alt: "an image of metallicas 4th album",
+    href: "https://www.metallica.com/"
+    }
+];
+
