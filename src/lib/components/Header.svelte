7013134d64384b94e7896eb0ea7b8418fc0bafaf
Brendan Malycky <bmalycky@hotmail.com>
2023-11-06 18:25:04 -0700

    created header component, addded to layout, created 2 routes

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  const navLinks = [
+    {name: "Home", href: "/"},
+    {name: "Albums", href: "/albums"},
+    {name: "Merch", href: "/merch"}
+  ]  
+</script>
+
+<header class="bg-gray-400 p-5">
+  <nav class="flex justify-center">
+    <ul class="flex">
+      {#each navLinks as {name, href}}
+        <li class="mx-4"><a class="p-2" {href}>{name}</a></li>
+      {/each}
+    </ul>
+  </nav>
+</header>
