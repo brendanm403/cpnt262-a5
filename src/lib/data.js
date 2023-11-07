a5f78cf485c3c5917a34215f4994e2d9780ddc1f
Brendan Malycky <bmalycky@hotmail.com>
2023-11-07 13:01:31 -0700

    edit data

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,22 +1,22 @@
-const albumData = [
+export const albumData = [
   {
   title: "Ride The Lightning", 
-  description: "The 2nd album by metallica, released 1984",
-  src: "",
+  description: "The 2nd album by metallica. The follow up album to kill em all and the first album wihtout Dave Mustaine",
+  src: "/images/RTL.png",
   alt: "an image of metallicas 2nd album",
   href: "https://www.metallica.com/"
     },
   {
   title: "Master Of Puppets", 
-  description: "The 3rd album by metallica, released 1986",
-  src: "",
+  description: "The 3rd album by metallica. It was the first metal recording to be preserved in the National Recording Registry",
+  src: "/images/MOP.jpg",
   alt: "an image of metallicas 3rd album",
   href: "https://www.metallica.com/"
     },
   {
     title: "...And Justice For All", 
-    description: "The 4th album by metallica, released 1988",
-    src: "",
+    description: "The 4th album by metallica. It is widely considered the bands last truly great thrash metal album.",
+    src: "/images/AJFA.jpg",
     alt: "an image of metallicas 4th album",
     href: "https://www.metallica.com/"
     }
